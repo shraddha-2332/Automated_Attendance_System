4fb08130609e0f18a329c85cbce1d368e3d41c4e
Shraddha Ukirade <shraddhaukirade@gmail.com>
2025-11-02 16:11:08 +0530

    Delete camera.py

--- camera.py
+++
@@ -1,9 +0,0 @@
-import cv2
-print("Scanning for available cameras...")
-for i in range(10):
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"✅ Camera found at index {i}")
-        cap.release()
-    else:
-        print(f"❌ No camera at index {i}")
